6afaf9da5508f56626ebebc6e542a4f4df2d798c
Leah Deshar <leah.desar@gmail.com>
2024-04-18 12:57:01 +0545

    Yahoo, error pinpointed!

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:habittracker/database/habit_database.dart';
 import 'package:habittracker/pages/home_page.dart';
 import 'package:habittracker/theme/theme_provider.dart';
 import 'package:provider/provider.dart';
 
+import 'database/habit_database.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await HabitDatabase.initialize();
-  await HabitDatabase().saveFirstLaunchDate();
-
+  // await HabitDatabase().saveFirstLaunchDate();
   runApp(MultiProvider(
     providers: [
-      ChangeNotifierProvider(create: (context) => HabitDatabase()),
-      ChangeNotifierProvider(create: (context) => ThemeProvider()),
+      ChangeNotifierProvider(create: (_) => HabitDatabase()),
+      ChangeNotifierProvider(create: (_) => ThemeProvider()),
     ],
     child: const MyApp(),
   ));
@@ -20,13 +20,12 @@ void main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'HabitForge',
-        debugShowCheckedModeBanner: false,
-        theme: Provider.of<ThemeProvider>(context).themeData,
-        home: HomePage());
+      debugShowCheckedModeBanner: false,
+      theme: Provider.of<ThemeProvider>(context).themeData,
+      home: const HomePage(),
+    );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:habittracker/components/my_drawer.dart';
-import 'package:habittracker/database/habit_database.dart';
-import 'package:habittracker/models/habit.dart';
-import 'package:habittracker/utli/habit_util.dart';
-import 'package:provider/provider.dart';
+// import 'package:habittracker/database/habit_database.dart';
+// import 'package:habittracker/models/habit.dart';
+// import 'package:habittracker/utli/habit_util.dart';
+// import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,11 +13,11 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  @override
-  void initState() {
-    Provider.of<HabitDatabase>(context, listen: false).readHabit();
-    super.initState();
-  }
+  // @override
+  // void initState() {
+  //   Provider.of<HabitDatabase>(context, listen: false).readHabit();
+  //   super.initState();
+  // }
 
   final TextEditingController textController = TextEditingController();
   void createNewHabit() {
@@ -33,9 +33,9 @@ class _HomePageState extends State<HomePage> {
               actions: [
                 MaterialButton(
                   onPressed: () {
-                    String newHabitName = textController.text;
-                    context.read<HabitDatabase>().addHabit(newHabitName);
-                    Navigator.pop(context);
+                    // String newHabitName = textController.text;
+                    // context.read<HabitDatabase>().addHabit(newHabitName);
+                    // Navigator.pop(context);
                     textController.clear();
                   },
                   child: const Text('Save'),
@@ -56,7 +56,7 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(),
       backgroundColor: Theme.of(context).colorScheme.background,
-      drawer: MyDrawer(),
+      drawer: const MyDrawer(),
       floatingActionButton: FloatingActionButton(
         onPressed: createNewHabit,
         elevation: 0,
@@ -66,22 +66,22 @@ class _HomePageState extends State<HomePage> {
           color: Colors.black,
         ),
       ),
-      body: _buildHabitList(),
+      // body: _buildHabitList(),
     );
   }
 
-  Widget _buildHabitList() {
-    final habitDatbase = context.watch<HabitDatabase>();
-    List<Habit> currentHabits = habitDatbase.currentHabits;
-    return ListView.builder(
-      itemCount: currentHabits.length,
-      itemBuilder: (context, index) {
-        final habit = currentHabits[index];
-        bool isCompletedToday = isHabitCompletedToday(habit.completedDays);
-        return ListTile(
-          title: Text(habit.name),
-        );
-      },
-    );
-  }
+  // Widget _buildHabitList() {
+  //   final habitDatbase = context.watch<HabitDatabase>();
+  //   List<Habit> currentHabits = habitDatbase.currentHabits;
+  //   return ListView.builder(
+  //     itemCount: currentHabits.length,
+  //     itemBuilder: (context, index) {
+  //       final habit = currentHabits[index];
+  //       bool isCompletedToday = isHabitCompletedToday(habit.completedDays);
+  //       return ListTile(
+  //         title: Text(habit.name),
+  //       );
+  //     },
+  //   );
+  // }
 }
